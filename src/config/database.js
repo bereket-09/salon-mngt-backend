27c2bd3a08e0dd1f07df3f9bfffb1f18129c130d
bereket.zelalem <bereket.zelalem@safaricom.et>
2026-05-11 18:44:27 +0300

    Updated db connection string url

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const dbConfig = {
-  url: process.env.DATABASE_URL,
+  url: process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_rAtWioa67UPS@ep-lucky-morning-aqjrz66l-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require&uselibpqcompat=true',
   dialect: 'postgres',
   logging: false,
   dialectOptions: {
